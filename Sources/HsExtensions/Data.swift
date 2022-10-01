a916c373740b157433c626c6814c3a85ae168f2a
ant013 <ant013@mail.ru>
2022-10-01 14:59:05 +0600

    Add VarInt to extensions

--- Sources/HsExtensions/Data.swift
+++ Sources/HsExtensions/Data.swift
@@ -28,4 +28,26 @@ public extension HsExtension where Base == Data {
         base.withUnsafeBytes { $0.baseAddress!.assumingMemoryBound(to: T.self).pointee }
     }
 
+    func to(type: String.Type) -> String {
+        String(bytes: base, encoding: .ascii)!.replacingOccurrences(of: "\0", with: "")
+    }
+
+    func to(type: VarInt.Type) -> VarInt {
+        let value: UInt64
+        let length = base[0..<1].hs.to(type: UInt8.self)
+        switch length {
+        case 0...252:
+            value = UInt64(length)
+        case 0xfd:
+            value = UInt64(base[1...2].hs.to(type: UInt16.self))
+        case 0xfe:
+            value = UInt64(base[1...4].hs.to(type: UInt32.self))
+        case 0xff:
+            fallthrough
+        default:
+            value = base[1...8].hs.to(type: UInt64.self)
+        }
+        return VarInt(value)
+    }
+
 }
