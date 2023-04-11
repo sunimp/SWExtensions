67d5dbb0e0e306a5c45c33321aef0c95eb14748e
Ermat <ealymbaev@gmail.com>
2023-04-11 15:09:12 +0600

    Add `PostPublished` and `DistinctPublished` property wrappers

---
+++ Sources/HsExtensions/Published.swift
@@ -0,0 +1,36 @@
+import Foundation
+import Combine
+
+@propertyWrapper
+public class PostPublished<Value> {
+    private let subject = PassthroughSubject<Value, Never>()
+    public let projectedValue: AnyPublisher<Value, Never>
+
+    public var wrappedValue: Value {
+        didSet {
+            subject.send(wrappedValue)
+        }
+    }
+
+    public init(wrappedValue: Value) {
+        self.wrappedValue = wrappedValue
+        projectedValue = subject.eraseToAnyPublisher()
+    }
+}
+
+@propertyWrapper
+public class DistinctPublished<Value> where Value: Equatable {
+    private let subject = PassthroughSubject<Value, Never>()
+    public let projectedValue: AnyPublisher<Value, Never>
+
+    public var wrappedValue: Value {
+        didSet {
+            subject.send(wrappedValue)
+        }
+    }
+
+    public init(wrappedValue: Value) {
+        self.wrappedValue = wrappedValue
+        projectedValue = subject.removeDuplicates().eraseToAnyPublisher()
+    }
+}
