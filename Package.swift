dabe384d726429b4edfb26dc1d7792c2239bebd1
杨光 <yangguang@webull.com>
2024-08-27 13:08:25 +0800

    fix: bump to ios 14.0

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "WWExtensions.Swift",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v14),
     ],
     products: [
         .library(
